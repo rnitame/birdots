73cc87924c6a1924a67432a2aef2b6352261fb7c
Ryo Nitami <nitry.n2t1m1@rnita.me>
2016-12-28 00:03:26 +0900

    fix: #43 #44

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,10 +1,10 @@
-# for vim-go
-eval "$(hub alias -s)"
+# OS ごとの処理
+case ${OSTYPE} in
+    darwin*)
+        export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+    ;;
+esac
 
-# 毎回 hub を呼ばないようにする
-local git==git
-
-export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 mkdir -p ~/.vim/colors
 cp ~/.vim/dein/repos/github.com/reedes/vim-colors-pencil/colors/pencil.vim ~/.vim/colors/pencil.vim
 
