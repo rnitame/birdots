67fd3bc907c3b27688133d48f5d46a085e17fade
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-05-01 08:37:59 +0900

    Fix #54: cp brewfile4linux

--- land.sh
+++ land.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-cp tmux/tmux.conf ~/.tmux.conf
 cp zsh/zshrc.zsh ~/.zshrc
 cp vim/vimrc ~/.vimrc
 cp gitfile/gitignore ~/.gitignore
 cp gitfile/gitconfig ~/.gitconfig
 cp gitfile/gitmessage ~/.gitmessage
-cp brew/Brewfile ~/.brewfile
 
 if [ "$(uname)" == 'Darwin' ]; then
-    cp tmux/tmux.conf.osx ~/.tmux.conf.osx
+    cp tmux/tmux.conf.osx ~/.tmux.conf
+    cp brew/Brewfile ~/.brewfile
+fi
+
+if [ "$(uname)" == 'Linux' ]; then
+    cp tmux/tmux.conf ~/.tmux.conf
+    cp brew/Brewfile4Linux ~/.brewfile
 fi
