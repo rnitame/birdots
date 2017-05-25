9a33eff658d6eebeaac161445d999eb70193da62
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-05-25 15:27:43 +0900

    copy tmux.conf

--- land.sh
+++ land.sh
@@ -5,13 +5,13 @@ cp vim/vimrc ~/.vimrc
 cp gitfile/gitignore ~/.gitignore
 cp gitfile/gitconfig ~/.gitconfig
 cp gitfile/gitmessage ~/.gitmessage
+cp tmux/tmux.conf ~/.tmux.conf
 
 if [ "$(uname)" == 'Darwin' ]; then
-    cp tmux/tmux.conf.osx ~/.tmux.conf
+    cp tmux/tmux.conf.osx ~/.tmux.conf.osx
     cp brew/Brewfile ~/.brewfile
 fi
 
 if [ "$(uname)" == 'Linux' ]; then
-    cp tmux/tmux.conf ~/.tmux.conf
     cp brew/Brewfile4Linux ~/.brewfile
 fi
