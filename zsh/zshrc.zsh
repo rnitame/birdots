4c6fb4c87c9fd53bc4a5441b87abdb485c9e719f
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2019-09-12 18:06:34 +0900

    Migrate zplug to zplugin

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -16,80 +16,24 @@ mkdir -p ~/Developer
 mkdir -p ~/.vim/colors
 cp ~/.vim/dein/repos/github.com/reedes/vim-colors-pencil/colors/pencil.vim ~/.vim/colors/pencil.vim
 
-# zplug
+# zplugin
 ## インストールチェック
-if [[ ! -d ~/.zplug ]]; then
-    git clone https://github.com/zplug/zplug ~/.zplug
+if [[ ! -d ~/.zplugin ]]; then
+    git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
 fi
 
-source ~/.zplug/init.zsh
-# defer must be 3 or less
-zplug "zsh-users/zsh-syntax-highlighting"
-zplug "stedolan/jq", \
-    from:gh-r, \
-    as:command, \
-    rename-to:jq
-zplug "b4b4r07/enhancd", \
-    use:init.sh
-zplug "denysdovhan/spaceship-prompt", \
-    use:spaceship.zsh, \
-    from:github, \
-    as:theme
-zplug "zsh-users/zsh-autosuggestions"
+source ~/.zplugin/bin/zplugin.zsh
+autoload -Uz _zplugin
+(( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-## インストールしてないプラグインがあればインストール
-if ! zplug check --verbose; then
-    printf "zplug plugin Install? [y/N] : "
-    if read -q; then
-        echo; zplug install
-    fi
-fi
+zplugin light zsh-users/zsh-syntax-highlighting
+zplugin light b4b4r07/enhancd
+zplugin light denysdovhan/spaceship-prompt
+zplugin light zsh-users/zsh-autosuggestions
 
-## プラグイン読み込み、コマンドパス追加
-zplug load --verbose
+autoload -U compinit
+compinit
 
-###############
-# zsh Setting 
-###############
-
-###############
-# ヒストリ関連
-###############
-# 履歴の保存先
-HISTFILE=$HOME/.zsh-history
-## メモリに展開する履歴の数
-# HISTSIZE=10000
-## 保存する履歴の数
-SAVEHIST=10000
-
-## コマンドラインの先頭がスペースで始まる場合ヒストリに追加しない
-setopt hist_ignore_space
-## history (fc -l) コマンドをヒストリリストから取り除く。
-setopt hist_no_store
-## 直前と同じコマンドをヒストリに追加しない
-setopt hist_ignore_dups
-## ヒストリを呼び出してから実行する間に一旦編集
-setopt hist_verify
-## zsh の開始, 終了時刻をヒストリファイルに書き込む
-setopt extended_history
-## 余分な空白は詰めて記録
-setopt hist_reduce_blanks  
-## 古いコマンドと同じものは無視 
-setopt hist_save_no_dups
-## ヒストリに追加されるコマンド行が古いものと同じなら古いものを削除
-setopt hist_ignore_all_dups
-## 補完時にヒストリを自動的に展開         
-setopt hist_expand
-
-## Screenでのコマンド共有用
-## シェルを横断して.zshhistoryに記録
-setopt inc_append_history
-## ヒストリを共有
-setopt share_history
-
-###################
-# ディレクトリ変更
-###################
 ## ディレクトリ名だけで cd
 setopt auto_cd
 ## cd 時に自動で push
@@ -97,17 +41,6 @@ setopt auto_pushd
 ## 同じディレクトリを pushd しない
 setopt pushd_ignore_dups
 
-############
-# 補間関連
-############
-## 補完機能の有効
-autoload -U compinit
-compinit
-
-## TAB で順に補完候補を切り替える
-setopt auto_menu
-## サスペンド中のプロセスと同じコマンド名を実行した場合はリジューム
-setopt auto_resume
 ## 補完候補を一覧表示
 setopt auto_list
 ## カッコの対応などを自動的に補完
@@ -120,27 +53,12 @@ zstyle ':completion:*:default' menu select=1
 eval `dircolors`
 export ZLS_COLORS=$LS_COLORS
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-## 補完候補一覧でファイルの種別をマーク表示
-setopt list_types
-## 補完候補を詰めて表示
-setopt list_packed
 
-#########
-# グロブ
-#########
-## 拡張グロブを使用
-## ファイル名で #, ~, ^ の 3 文字を正規表現として扱う
-setopt extended_glob
-## =command を command のパス名に展開する
-setopt equals
 ## --prefix=/usr などの = 以降も補完
 setopt magic_equal_subst
 ## ファイル名の展開で辞書順ではなく数値的にソート
 setopt numeric_glob_sort
 
-##########
-# 入出力
-##########
 ## 出力時8ビットを通す
 setopt print_eight_bit
 ## スペルチェック。間違うと訂正してくれる
@@ -160,24 +78,9 @@ setopt mark_dirs
 ## 最後のスラッシュを自動的に削除しない
 setopt noautoremoveslash
 
-##################
-# プロンプト関連
-##################
 # 色有効
 autoload -U colors
 colors
-
-##############
-# ジョブ制御
-##############
-## 内部コマンド jobs の出力をデフォルトで jobs -l にする
-setopt long_list_jobs
-
-#################
-# その他・未分類
-#################
-## コアダンプサイズを制限
-# limit coredumpsize 102400
 
 ## ビープを鳴らさない
 setopt nobeep
