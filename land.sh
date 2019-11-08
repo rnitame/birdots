1992792311948603f883c31940e1d72029098022
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2019-11-08 18:48:34 +0900

    Implove hyper.js settings!

--- land.sh
+++ land.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+## Homebrew インストール
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 cp zsh/zshrc.zsh ~/.zshrc
 cp zsh/starship.toml ~/.config/starship.toml
 cp gitfile/gitconfig ~/.gitconfig
 cp gitfile/gitmessage ~/.gitmessage
-cp brew/Brewfile ~/.brewfile
+cp brew/Brewfile ~/.Brewfile
 cp hyper.js ~/.hyper.js
+
+## brewfile に書いたソフトウェアたちをインストール
+brew bundle --global 
+brew bundle cleanup
 
 ## SpaceVim インストール
 curl -sLf https://spacevim.org/install.sh | bash
