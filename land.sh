e935faa1dcd15cab8c3c8ffd71c63acd95194ef8
Ryo Nitami <nitry.n2t1m1+github@gmail.com>
2019-11-11 14:32:41 +0900

    Add SpaceVim settings

--- land.sh
+++ land.sh
@@ -3,19 +3,20 @@
 ## Homebrew インストール
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+## SpaceVim インストール
+curl -sLf https://spacevim.org/install.sh | bash
+
+## zplugin インストール
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
+
 cp zsh/zshrc.zsh ~/.zshrc
 cp zsh/starship.toml ~/.config/starship.toml
 cp gitfile/gitconfig ~/.gitconfig
 cp gitfile/gitmessage ~/.gitmessage
 cp brew/Brewfile ~/.Brewfile
 cp hyper.js ~/.hyper.js
+cp vim/space_vim.toml ~/.SpaceVim.d/init.toml
 
 ## brewfile に書いたソフトウェアたちをインストール
 brew bundle --global 
 brew bundle cleanup
-
-## SpaceVim インストール
-curl -sLf https://spacevim.org/install.sh | bash
-
-## zplugin インストール
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
